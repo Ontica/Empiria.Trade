cae2fcff8568091a70e423b0386f91048f28923d
José Manuel Cota <jmcota@ontica.org>
2023-08-11 13:04:21 -0400

    Initial product related classes were added

---
+++ Products/Domain/Product.cs
@@ -0,0 +1,73 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Products.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : Product                                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a product.                                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+using Empiria.Ontology;
+
+namespace Empiria.Trade.Products {
+
+  /// <summary>Represents a product.</summary>
+  [PartitionedType(typeof(ProductType))]
+  public partial class Product : BaseObject {
+
+    #region Constructors and parsers
+
+    protected Product(ProductType productType) : base(productType) {
+      // Required by Empiria Framework for all partitioned types.
+    }
+
+
+    internal Product(ProductType productType,
+                     ProductFields data) : base(productType) {
+      this.LoadData(data);
+    }
+
+
+    static public Product Parse(int id) => BaseObject.ParseId<Product>(id);
+
+    static public Product Parse(int id, bool reload) => BaseObject.ParseId<Product>(id, reload);
+
+    static public Product Parse(string uid) => BaseObject.ParseKey<Product>(uid);
+
+    static public Product Empty => BaseObject.ParseEmpty<Product>();
+
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public ProductType ProductType {
+      get {
+        return (ProductType) base.GetEmpiriaType();
+      }
+    }
+
+
+
+    internal string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(this.ProductType.DisplayName);
+      }
+    }
+
+
+    #endregion Properties
+
+    #region Methods
+
+    private void LoadData(ProductFields data) {
+      throw new NotImplementedException();
+    }
+
+    #endregion Methods
+
+  }  // class Product
+
+}  // namespace Empiria.Trade.Products
---
+++ Products/Domain/ProductFields.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Products.dll                 Pattern   : Information Holder                      *
+*  Type     : ProductFields                              License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds a product attributes list.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Trade.Products {
+
+  /// <summary>Holds a product attributes list. </summary>
+  internal class ProductFields {
+
+  }  // class ProductFields
+
+}  // namespace Empiria.Trade.Products
