cbb17a217f98026727acd767de7e5b0f15338e0e
cyanez <cyanez@ontica.org>
2023-09-26 19:36:39 -0600

    WebApi party added

---
+++ WebApi/Core/PartyController.cs
@@ -0,0 +1,77 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Web Api                                 *
+*  Assembly : Empiria.Trade.WebApi.Inventory.dll         Pattern   : Controller                              *
+*  Type     : TRDProductController                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Query web API used to retrieve TRDProducts.                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+
+using Empiria.Trade.Inventory.Products.Adapters;
+using Empiria.Trade.Inventory.Products.UseCases;
+
+using Empiria.Trade.Core.Adapters;
+using Empiria.Trade.Core.UsesCases;
+
+using Empiria.WebApi;
+
+
+namespace Empiria.Trade.WebApi.Core {
+  internal class PartyController: WebApiController {
+
+
+    #region Web Apis
+        
+    [HttpGet]
+    [Route("v4/trade/contacts/customers")]
+    public CollectionModel GetCustomers([FromUri] string keywords) {
+
+      using (var usecases = PartyUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> customers = usecases.GetCustomers(keywords);
+
+        return new CollectionModel(base.Request, customers);
+      }
+    }
+
+    [HttpGet]
+    [Route("v4/trade/contacts/customer-contacts/{customerUID:guid}")]
+    public CollectionModel GetCustomerContacts([FromUri] string customerUID) {
+
+      using (var usecases = PartyUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> customerContacts = usecases.GetCustomerContacts(customerUID);
+
+        return new CollectionModel(base.Request, customerContacts);
+      }
+    }
+
+    [HttpGet]
+    [Route("v4/trade/contacts/suppliers")]
+    public CollectionModel GetSuppliers([FromUri] string keywords) {
+
+      using (var usecases = PartyUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> suppliers = usecases.GetSuppliers(keywords);
+
+        return new CollectionModel(base.Request, suppliers);
+      }
+    }
+
+    [HttpGet]
+    [Route("v4/trade/contacts/sales-agents")]
+    public CollectionModel GetSalesAgents([FromUri] string keywords) {
+
+      using (var usescase = PartyUseCases.UseCaseInteractor()) {
+        FixedList<NamedEntityDto> salesAgents = usescase.GetSalesAgents(keywords);
+
+        return new CollectionModel(base.Request, salesAgents);
+      }
+    }
+
+    #endregion Web Apis
+
+
+
+  }
+}
