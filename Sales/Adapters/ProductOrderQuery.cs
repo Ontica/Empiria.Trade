5e813ce15b8e40c29b697f997ceaedf96aaa6ea1
efrain-jerv <efrain.rodriguez@ontica.org>
2023-10-18 13:32:15 -0700

    Product order entities were added to Sales folder

---
+++ Sales/Adapters/ProductOrderQuery.cs
@@ -0,0 +1,31 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Interface adapters                      *
+*  Assembly : Empiria.Trade.Sales.dll                    Pattern   : Data Transfer Object                    *
+*  Type     : ProductOrderQuery                          License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Query to filter Products by order.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Empiria.Trade.Orders;
+
+namespace Empiria.Trade.Sales.Adapters {
+
+  /// <summary>Query to filter Products by order.</summary>
+  public class ProductOrderQuery {
+
+
+    public string Keywords {
+      get; set;
+    } = string.Empty;
+
+
+    public SalesOrderDto Order {
+      get; set;
+    }
+
+
+  } // class ProductOrderQuery
+
+} // namespace Empiria.Trade.Sales.Adapters
---
+++ Sales/UseCases/ProductForOrderUseCases.cs
@@ -0,0 +1,55 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Use cases Layer                         *
+*  Assembly : Empiria.Trade.Orders.dll                   Pattern   : Use case interactor class               *
+*  Type     : ProductForOrderUseCases                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases used to management Products for order.                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Threading.Tasks;
+using Empiria.Services;
+using Empiria.Trade.Products.Adapters;
+using Empiria.Trade.Products.UseCases;
+
+namespace Empiria.Trade.Sales.UseCases {
+
+  /// <summary>Use cases used to management Products for order.</summary>
+  public class ProductForOrderUseCases : UseCase {
+
+
+    #region Constructors and parsers
+
+    protected ProductForOrderUseCases() {
+      // no-op
+    }
+
+    static public ProductForOrderUseCases UseCaseInteractor() {
+      return CreateInstance<ProductForOrderUseCases>();
+    }
+
+
+    #endregion Constructors and parsers
+
+
+    #region Use cases
+
+
+    public async Task<FixedList<IProductEntryDto>> GetProductsForOrder(ProductQuery keywords) {
+
+      using (var usecases = TRDProductUseCases.UseCaseInteractor()) {
+
+        FixedList<IProductEntryDto> productDto = await usecases.GetProductsList(keywords)
+                                                .ConfigureAwait(false);
+
+        return productDto;
+      }
+    }
+
+
+    #endregion Use cases
+
+  } // class ProductForOrderUseCases
+
+} // Use cases used to management Products.
