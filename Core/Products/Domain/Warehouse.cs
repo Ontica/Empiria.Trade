1f6194fd3af0cc408d51e9666215007d67f8c11f
efrain-jerv <efrain.rodriguez@ontica.org>
2023-11-29 00:55:54 -0700

    Warehouse and WarehouseBin entities were added to core

---
+++ Core/Products/Domain/Warehouse.cs
@@ -0,0 +1,87 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Products.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : Warehouse                                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a warehouse.                                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Trade.Products {
+
+  /// <summary>Represents a warehouse.</summary>
+  public class Warehouse : BaseObject {
+
+    #region Constructors and parsers
+
+    internal Warehouse() {
+
+    }
+
+
+    static public Warehouse Parse(int id) => ParseId<Warehouse>(id);
+
+    static public Warehouse Parse(int id, bool reload) => ParseId<Warehouse>(id, reload);
+
+    static public Warehouse Parse(string uid) => ParseKey<Warehouse>(uid);
+
+    static public Warehouse Empty => ParseEmpty<Warehouse>();
+
+
+    #endregion Constructors and parsers
+
+
+    #region Properties
+
+
+
+    [DataField("WarehouseUID")]
+    public string WarehouseUID {
+      get;
+      internal set;
+    }
+
+
+    [DataField("WarehouseCode")]
+    public string Code {
+      get;
+      internal set;
+    }
+
+
+    [DataField("WarehouseName")]
+    public string Name {
+      get;
+      internal set;
+    }
+
+
+    [DataField("Description")]
+    public string Description {
+      get;
+      internal set;
+    }
+
+
+    [DataField("OwnerId")]
+    public int OwnerId {
+      get;
+      internal set;
+    }
+
+
+    [DataField("CompanyId")]
+    public int CompanyId {
+      get;
+      internal set;
+    }
+
+
+    #endregion Properties
+
+
+  } // class Warehouse
+
+} // namespace Empiria.Trade.Products
---
+++ Core/Products/Domain/WarehouseBin.cs
@@ -0,0 +1,109 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Product Management                         Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Products.dll                 Pattern   : Partitioned Type / Information Holder   *
+*  Type     : WarehouseBin                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a warehouse bin.                                                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Trade.Products {
+
+  /// <summary>Represents a warehouse bin.</summary>
+  public class WarehouseBin : BaseObject {
+
+
+    #region Constructors and parsers
+
+    internal WarehouseBin() {
+
+    }
+
+
+    static public WarehouseBin Parse(int id) => ParseId<WarehouseBin>(id);
+
+    static public WarehouseBin Parse(int id, bool reload) => ParseId<WarehouseBin>(id, reload);
+
+    static public WarehouseBin Parse(string uid) => ParseKey<WarehouseBin>(uid);
+
+    static public WarehouseBin Empty => ParseEmpty<WarehouseBin>();
+
+
+    #endregion Constructors and parsers
+
+
+    #region Properties
+
+
+
+    [DataField("WarehouseBinUID")]
+    public string WarehouseBinUID {
+      get;
+      internal set;
+    }
+
+
+    [DataField("WarehouseId")]
+    public Warehouse Warehouse {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinCode")]
+    public string BinCode {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinShelf")]
+    public string BinShelf {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinRow")]
+    public string BinRow {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinColumn")]
+    public string BinColumn {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinDescription")]
+    public string BinDescription {
+      get;
+      internal set;
+    }
+
+
+    [DataField("AssignedVendorProductId")]
+    public ProductFields AssignedVendorProduct {
+      get;
+      internal set;
+    }
+
+
+    [DataField("BinStatus")]
+    public string BinStatus {
+      get;
+      internal set;
+    }
+
+
+    #endregion Properties
+
+
+  } // class WarehouseBin
+
+} // namespace Empiria.Trade.Products
