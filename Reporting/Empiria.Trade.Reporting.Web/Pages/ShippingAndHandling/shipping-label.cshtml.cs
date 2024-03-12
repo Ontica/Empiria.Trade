06e7cd0beee657e92b0919e20536481d49b89cf5
efrain-jerv <efrain.rodriguez@ontica.org>
2024-03-11 18:23:44 -0700

    Razor project for reporting was added to Trade

---
+++ Reporting/Empiria.Trade.Reporting.Web/Pages/ShippingAndHandling/shipping-label.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Empiria.Trade.Reporting.Web.Pages.ShippingAndHandling
+{
+    public class shipping_labelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
