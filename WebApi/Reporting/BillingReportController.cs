504e63e0614e1cf0e38a7e6d17980e3b651ee147
efrain-jerv <efrain.rodriguez@ontica.org>
2024-03-19 20:43:26 -0700

    Web apis for shipping label and billing reports were added to Trade.WebApi

--- WebApi/Reporting/BillingReportController.cs
+++ WebApi/Reporting/BillingReportController.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Shipping and handling Management              Component : Web Api                              *
+*  Assembly : Empiria.Trade.WebApi.ShippingAndHandling.dll  Pattern   : Controller                           *
+*  Type     : ShippingLabelsController                      License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Query web API used to manage billing reports.                                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Web.Http;
+using Empiria.Trade.Core.Catalogues;
+using Empiria.WebApi;
 
 namespace Empiria.Trade.WebApi.Reporting {
     
-    public class BillingReportController {
-    }
-}
+
+    /// <summary>Summary  : Query web API used to manage billing reports.</summary>
+    [AllowAnonymous]
+    public class BillingReportController : WebApiController {
+
+
+    #region Web apis
+
+
+
+    #endregion Web apis
+
+
+  } // public class BillingReportController
+
+} // namespace Empiria.Trade.WebApi.Reporting
--- WebApi/Reporting/ShippingLabelsController.cs
+++ WebApi/Reporting/ShippingLabelsController.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Shipping and handling Management              Component : Web Api                              *
+*  Assembly : Empiria.Trade.WebApi.ShippingAndHandling.dll  Pattern   : Controller                           *
+*  Type     : ShippingLabelsController                      License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Query web API used to manage shippings labels.                                                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Web.Http;
+using Empiria.Trade.Core.Catalogues;
+using Empiria.Trade.Sales.ShippingAndHandling.Adapters;
+using Empiria.Trade.Sales.ShippingAndHandling.UseCases;
+using Empiria.WebApi;
 
 namespace Empiria.Trade.WebApi.Reporting {
 
     
-    public class ShippingLabelsController {
+    /// <summary>Query web API used to manage shippings labels.</summary>
+    [AllowAnonymous]
+    public class ShippingLabelsController : WebApiController {
+
+
+    #region Web apis
+
+    //trade/reporting/shipping/{shippingUID}/billings
+
+    //trade/reporting/shipping/{shippingUID}/billing/{orderUID}
+
+    //trade/reporting/shipping/{shippingUID}/labels
+
+
+    [HttpGet]
+    [AllowAnonymous]
+    [Route("v4/trade/sales/shipping/{shippingUID}/labels")]
+    public CollectionModel GetShippingLabels([FromUri] string shippingUID) {
+
+      using (var usecases = ShippingLabelUseCases.UseCaseInteractor()) {
+
+        FixedList<ShippingLabel> shippingLabels = usecases.GetShippingLabels(shippingUID);
+
+        return new CollectionModel(this.Request, shippingLabels);
+      }
     }
-}
+
+
+    #endregion Web apis
+
+
+  } // public class ShippingLabelsController
+
+} // namespace Empiria.Trade.WebApi.Reporting
