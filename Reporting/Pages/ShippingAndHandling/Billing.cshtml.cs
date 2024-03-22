5c6c643bc21d3f1f6d3085d2e1a266785d3c2787
efrain-jerv <efrain.rodriguez@ontica.org>
2024-03-22 16:18:54 -0700

    Report views for shipping labels and billings were updated

--- Reporting/Pages/ShippingAndHandling/Billing.cshtml.cs
+++ Reporting/Pages/ShippingAndHandling/Billing.cshtml.cs
@@ -1,3 +1,5 @@
+using Empiria.Trade.Reporting.WebApi.Client.Adapters;
+using Empiria.Trade.Reporting.WebApi.Client.ShippingAndHandling;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace Reporting.Web.Pages.Shipping
 {
     public class BillingModel : PageModel
     {
-        public void OnGet()
-        {
+
+        public IEnumerable<ShippingLabelByPalletDto> Labels {
+            get; set;
+        } = new List<ShippingLabelByPalletDto>();
+
+
+        public async Task OnGet(string shippingUID) {
+            var controller = new ShippingLabelsWebApiClientController();
+
+            Labels = await controller.GetShippingLabelByPalletFromURI(shippingUID);
         }
     }
 }
