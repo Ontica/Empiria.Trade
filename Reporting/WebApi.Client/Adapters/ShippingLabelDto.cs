391abb372251164c560c18f0b1fbd5be66d7847f
efrain-jerv <efrain.rodriguez@ontica.org>
2024-04-10 16:41:53 -0700

    Properties to get shipping and delivery number for shippingLabel were added

--- Reporting/WebApi.Client/Adapters/ShippingLabelDto.cs
+++ Reporting/WebApi.Client/Adapters/ShippingLabelDto.cs
@@ -12,115 +12,125 @@ using System;
 namespace Empiria.Trade.Reporting.WebApi.Client.Adapters {
 
 
-    /// <summary>Output DTO used to return shipping label by pallet.</summary>
-    public class ShippingLabelByPalletDto {
+  /// <summary>Output DTO used to return shipping label by pallet.</summary>
+  public class ShippingLabelByPalletDto {
 
-        public string Company {
-            get; set;
-        } = string.Empty;
-
-
-        public string CompanyAddress {
-            get; set;
-        } = string.Empty;
+    public string Company {
+      get; set;
+    } = string.Empty;
 
 
-        public string ShippingNumber {
-            get; set;
-        } = string.Empty;
+    public string CompanyAddress {
+      get; set;
+    } = string.Empty;
 
 
-        public string ShippingType {
-            get; set;
-        } = string.Empty;
+    public string ShippingGuide {
+      get; set;
+    } = string.Empty;
 
 
-        public string ParcelSupplier {
-            get; set;
-        } = string.Empty;
+    public string ShippingNumber {
+      get; set;
+    } = string.Empty;
 
 
-        public string Customer {
-            get; set;
-        } = string.Empty;
+    public string DeliveryNumber {
+      get; set;
+    } = string.Empty;
 
 
-        public string CustomerAddress {
-            get; set;
-        } = string.Empty;
+    public string ShippingType {
+      get; set;
+    } = string.Empty;
 
 
-        public string CustomerPhoneNumber {
-            get; set;
-        } = string.Empty;
+    public string ParcelSupplier {
+      get; set;
+    } = string.Empty;
 
 
-        public string PackingName {
-            get; set;
-        } = string.Empty;
+    public string Customer {
+      get; set;
+    } = string.Empty;
 
 
-        public string PackingCount {
-            get; set;
-        } = string.Empty;
+    public string CustomerAddress {
+      get; set;
+    } = string.Empty;
 
 
-        public int PackageQuantity {
-            get; set;
-        }
+    public string CustomerPhoneNumber {
+      get; set;
+    } = string.Empty;
 
 
-        public int TiedQuantity {
-            get; set;
-        }
+    public string PackingName {
+      get; set;
+    } = string.Empty;
 
 
-        public int BagQuantity {
-            get; set;
-        }
-
-    } // class ShippingLabelByPallet
+    public string PackingCount {
+      get; set;
+    } = string.Empty;
 
 
-    /// <summary>Output DTO used to return shipping label.</summary>
-    public class SupplyLabelDto {
+    public int PackageQuantity {
+      get; set;
+    }
 
 
-        public string orderUID {
-            get; set;
-        } = string.Empty;
+    public int TiedQuantity {
+      get; set;
+    }
 
 
-        public string productCode {
-            get; set;
-        } = string.Empty;
+    public int BagQuantity {
+      get; set;
+    }
+
+  } // class ShippingLabelByPallet
 
 
-        public string productPresentation {
-            get; set;
-        } = string.Empty;
+  /// <summary>Output DTO used to return shipping label.</summary>
+  public class SupplyLabelDto {
 
 
-        public string description {
-            get; set;
-        } = string.Empty;
+    public string orderUID {
+      get; set;
+    } = string.Empty;
 
 
-        public string ubication {
-            get; set;
-        } = string.Empty;
+    public string productCode {
+      get; set;
+    } = string.Empty;
 
 
-        public string comments {
-            get; set;
-        } = string.Empty;
+    public string productPresentation {
+      get; set;
+    } = string.Empty;
 
 
-        public decimal quantity {
-            get; set;
-        }
+    public string description {
+      get; set;
+    } = string.Empty;
 
 
-    } // class ShippingLabelDto
+    public string ubication {
+      get; set;
+    } = string.Empty;
+
+
+    public string comments {
+      get; set;
+    } = string.Empty;
+
+
+    public decimal quantity {
+      get; set;
+    }
+
+
+  } // class ShippingLabelDto
 
 } // namespace Empiria.Trade.Reporting.WebApi.Client.Adapters
