c4fd0e4116c410d2583de91877a0a65dcbf618e2
efrain-jerv <efrain.rodriguez@ontica.org>
2024-06-25 18:43:50 -0700

    Inventory reporting structure and domain entities added to Trade.Inventory

---
+++ Inventory/Reporting/Domain/InventoryReportsBuilder.cs
@@ -0,0 +1,35 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Inventory Management                       Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Inventory.dll                Pattern   : Partitioned Type / Information Holder   *
+*  Type     : InventoryReportsBuilder                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Generate data for Inventory order.                                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Trade.Inventory.Domain {
+
+
+  /// <summary></summary>
+  internal class InventoryReportsBuilder {
+
+
+    #region Public methods
+
+
+    internal FixedList<InventoryStockEntry> GetInventoryStock() {
+
+
+
+      return new FixedList<InventoryStockEntry>();
+    }
+
+
+    #endregion Public methods
+
+
+  } // class InventoryReportsBuilder
+
+} // namespace Empiria.Trade.Inventory.Domain
---
+++ Inventory/Reporting/Domain/InventoryStockEntry.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria Trade *********************************************************************************************
+*                                                                                                            *
+*  Module   : Inventory Management                       Component : Domain Layer                            *
+*  Assembly : Empiria.Trade.Inventory.dll                Pattern   : Information Holder                      *
+*  Type     : InventoryStockEntry                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents an inventory stock entry.                                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using Empiria.Trade.Core;
+using Empiria.Trade.Core.Catalogues;
+using Empiria.Trade.Inventory.Adapters;
+using Empiria.Trade.Products;
+
+namespace Empiria.Trade.Inventory {
+
+
+  /// <summary>Represents an inventory stock entry.</summary>
+  public class InventoryStockEntry : SalesInventoryStock {
+
+
+    #region Constructors and parsers
+
+    internal InventoryStockEntry() {
+      // no-op
+    }
+
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+
+
+    #endregion Properties
+
+  } // class InventoryStockEntry
+
+} // namespace Empiria.Trade.Inventory
