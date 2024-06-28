5b627caff2d315edef289a8edb7163f234115741
efrain-jerv <efrain.rodriguez@ontica.org>
2024-06-28 15:48:28 -0700

    Enums to manage report types added to Trade.Core.Commons

---
+++ Core/Common/GeneralEnums.cs
@@ -0,0 +1,35 @@
+﻿/* Empiria Core  *********************************************************************************************
+*                                                                                                            *
+*  Module   : Trade Core                                   Component : Entity control enumerations           *
+*  Assembly : Empiria.Trade.Core.dll                       Pattern   : Enumeration                           *
+*  Type     : GeneralEnums                                 License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Enums to assing item type for reports elements.                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+
+namespace Empiria.Trade.Core {
+  
+  
+  internal class GeneralEnums {
+
+  } // class GeneralEnums
+
+
+  /// <summary>Enums to assing item type for reports elements.</summary>
+  public enum ReportItemType {
+
+    Header,
+
+    Entry,
+
+    Summary,
+
+    Group,
+
+    Total
+
+  } // enum ReportItemType
+
+} // namespace Empiria.Trade.Core
